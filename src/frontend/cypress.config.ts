bec8991798fa70ea527c379beaf93344632f5f94
alexgRapDev <90631541+alexgRapDev@users.noreply.github.com>
2026-03-12 14:42:00 -0400

    added datadog config

--- src/frontend/cypress.config.ts
+++ src/frontend/cypress.config.ts
@@ -6,6 +6,28 @@ import dotEnv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 import { resolve } from 'path';
 
+import {datadogRum} from '@datadog/browser-rum'
+                    
+datadogRum.init({
+    applicationId: '5edf844c-662c-4786-8e7b-949cb113037b',
+    clientToken: 'pubbf94e78e831a1cdc6fd091780301c04d',
+    site: 'datadoghq.com',
+    service: 'otel-frontend',
+    env: 'dev',
+    version: '1.0.0',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackResources: true,
+    trackUserInteractions: true,
+    trackLongTasks: true,
+
+                   
+    // ----- Recommended Options -----
+    // Do not forget to upload your debug symbols to get deobfuscated stack traces, read {docs} to learn how to
+    // allowedTracingUrls: '<BACKEND-URL>',
+    // defaultPrivacyLevel: 'mask-user-input',
+});
+
 const myEnv = dotEnv.config({
   path: resolve(__dirname, '../../.env'),
 });
